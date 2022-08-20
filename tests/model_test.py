9a37e36df0301a4594b8f96fa2b8a883dd6f703d
czert <pyhovoleg31@gmail.com>
2022-08-20 14:41:28 +0500

    LaszyEvaluation class & tests

--- tests/model_test.py
+++ tests/model_test.py
@@ -3,10 +3,63 @@ from codiri.src.model.results import Results
 from codiri.src.model.reference import IReference
 from codiri.src.database import InMemoryDatabase
 from codiri.src.model.input import BaseInput, Input
+from codiri.src.model.lazy_eval import LazyEvaluation
 import math
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock, patch, call
 import pytest
 import unittest
+
+
+class TestLazyEvaluation(unittest.TestCase):
+    def test_do_not_exec_on_creation(self):
+        self.assertEqual(LazyEvaluation(lambda: None).results, dict())
+        self.assertEqual(LazyEvaluation(lambda: 1).results, dict())
+        self.assertEqual(LazyEvaluation(lambda x: x).results, dict())
+        self.assertEqual(LazyEvaluation(lambda x, y: x * y).results, dict())
+
+    def test_exec(self):
+        self.assertEqual(LazyEvaluation(lambda: None).exec(), None)
+        self.assertEqual(LazyEvaluation(lambda: 1).exec(), 1)
+        self.assertEqual(LazyEvaluation(lambda x: x).exec((1,)), 1)
+        self.assertEqual(LazyEvaluation(lambda x: x**2).exec((2,)), 4)
+        self.assertEqual(LazyEvaluation(lambda x, y: x**y).exec((2, 3)), 8)
+
+    def assertCallRaises(self, errorType, call):
+        with self.assertRaises(errorType):
+            call()
+
+    def test_exec_wrong_params(self):
+        self.assertCallRaises(
+            TypeError, lambda: LazyEvaluation(lambda: None).exec(1)
+        )
+        self.assertCallRaises(
+            TypeError, lambda: LazyEvaluation(lambda: None).exec((1,))
+        )
+        self.assertCallRaises(
+            TypeError, lambda: LazyEvaluation(lambda x: None).exec()
+        )
+        self.assertCallRaises(
+            TypeError, lambda: LazyEvaluation(lambda x: None).exec((1, 2))
+        )
+
+    def test_cumulative_results(self):
+        evalution = LazyEvaluation(lambda x: x**2)
+        self.assertEqual(evalution.exec((1,)), 1)
+        self.assertEqual(evalution.results, {(1,): 1})
+        self.assertEqual(evalution.exec((2,)), 4)
+        self.assertEqual(evalution.results, {(1,): 1, (2,): 4})
+        self.assertEqual(evalution.exec((3,)), 9)
+        self.assertEqual(evalution.results, {(1,): 1, (2,): 4, (3,): 9})
+
+    def test_exec_with_same_args(self):
+        mock = MagicMock(return_value=None)
+        evalution = LazyEvaluation(mock)
+        evalution.exec((1,))
+        evalution.exec((1,))
+        mock.assert_called_once_with(1)
+        evalution.exec((2,))
+        mock.assert_has_calls([call(1), call(2)])
+        self.assertEqual(evalution.results, {(1,): None, (2,): None})
 
 
 class TestBaseInput(unittest.TestCase):
