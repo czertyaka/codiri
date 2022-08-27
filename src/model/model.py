e33ce629e96e3849285cb58bab05b3bbb8d61303
czert <pyhovoleg31@gmail.com>
2022-08-27 18:02:45 +0500

    deleted dead code

--- src/model/model.py
+++ src/model/model.py
@@ -1,6 +1,5 @@
 from .common import pasquill_gifford_classes, log, ValidatingMap
 from .input import Input
-from .results import Results
 from .reference import IReference
 from .constraints import IConstraints, ConstraintsComplianceError
 from .lazy_eval import LazyEvaluation as LEval
@@ -25,7 +24,6 @@ from .formulas import (
 )
 import math
 import numpy as np
-from scipy import integrate
 from ..activity import blowout_activity_flow
 from typing import Tuple, Dict
 
@@ -83,7 +81,6 @@ class Model:
             raise ValueError("invalid reference instance")
         self._reference = reference
         self._constraints = DefaultConstraints(reference.all_nuclides())
-        self._results = Results()
 
     def calculate(self, inp: Input) -> bool:
         """Execute calculations
@@ -389,150 +386,3 @@ class Model:
             * self.input.blowout_time
             * math.pow(self.input.square_side, 2)
         )
-
-    def __calculate_sediment_detachments(self, nuclide):
-        """РБ-134-17, p. 29, (17)"""
-
-        precipitation_rate = self.input.precipitation_rate
-        standard_washing_capacity = self.reference.standard_washing_capacity(
-            nuclide
-        )
-        unitless_washing_capacity = self.reference.unitless_washing_capacity
-
-        value = (
-            precipitation_rate
-            * standard_washing_capacity
-            * unitless_washing_capacity
-        )
-
-        self.results.sediment_detachments.insert(nuclide, value)
-
-    def __calculate_height_deposition_factors(self, nuclide: str):
-        """РБ-134-17, p. 28, (13)"""
-
-        windspeeds = self.input.extreme_windspeeds
-        side_half = self.input.square_side / 2
-        depletions = self.results.full_depletions[nuclide]
-        values = dict()
-
-        for a_class in pasquill_gifford_classes:
-            factor = depletions[a_class] / (
-                math.sqrt(math.pi)
-                * windspeeds[a_class]
-                * 4
-                * math.pow(side_half, 2)
-            )
-
-            diffusion_coefficients = self.reference.diffusion_coefficients(
-                a_class
-            )
-
-            def subintegral_func(x):
-                s_y = self._calculate_dispersion_coefficients(
-                    diffusion_coefficients, self.input.distance - x
-                )["y"]
-                return math.erf(side_half / (math.sqrt(2) * s_y))
-
-            values[a_class] = (
-                factor
-                * integrate.quad(subintegral_func, -side_half, side_half)[0]
-            )
-
-        self.results.height_deposition_factors.insert(nuclide, values)
-
-    def __calculate_dispersion_coefficients(
-        self, diffusion_coefficients: dict, x: float
-    ) -> dict:
-        """РБ-134-17, p. 29, (19) (20)"""
-
-        p_z = diffusion_coefficients["p_z"]
-        q_z = diffusion_coefficients["q_z"]
-        p_y = diffusion_coefficients["p_y"]
-        q_y = diffusion_coefficients["q_y"]
-
-        z = p_z * math.pow(x, q_z)
-        y = (
-            p_y * math.pow(x, q_y)
-            if x < 10000
-            else p_y * math.pow(1000, q_y - 0.5) * math.sqrt(x)
-        )
-
-        return dict(z=z, y=y)
-
-    def __calculate_depletions(self, nuclide: str) -> None:
-        self._calculate_rad_depletions(nuclide)
-        self._calculate_dry_depletions(nuclide)
-        self._calculate_wet_depletions(nuclide)
-        self._calculate_full_depletions(nuclide)
-
-    def __calculate_rad_depletions(self, nuclide: str) -> None:
-        """РБ-134-17, p. 28, (14)"""
-
-        decay_coeff = self.reference.radio_decay_coeff(nuclide)
-        windspeeds = self.input.extreme_windspeeds
-        dist = self.input.distance
-        values = dict()
-
-        for a_class in pasquill_gifford_classes:
-            values[a_class] = math.exp(
-                -decay_coeff * dist / windspeeds[a_class]
-            )
-
-        self.results.rad_depletions.insert(nuclide, values)
-
-    def __calculate_dry_depletions(self, nuclide: str) -> None:
-        """РБ-134-17, p. 28, (15)"""
-
-        windspeeds = self.input.extreme_windspeeds
-        depositon_rate = self.reference.deposition_rate(nuclide)
-        h_rel = self.reference.terrain_roughness(self.input.terrain_type)
-        dist = self.input.distance
-        values = dict()
-
-        for a_class in pasquill_gifford_classes:
-            factor = (
-                math.sqrt(2 / math.pi) * depositon_rate / windspeeds[a_class]
-            )
-            diffusion_coefficients = self.reference.diffusion_coefficients(
-                a_class
-            )
-
-            def subintegral_func(x: float) -> float:
-                sigma_z = self._calculate_dispersion_coefficients(
-                    diffusion_coefficients, x
-                )["z"]
-                exp_factor = -math.pow((h_rel / sigma_z), 2) / 2
-                return math.exp(exp_factor) / sigma_z
-
-            integral = integrate.quad(subintegral_func, 0, dist)[0]
-            values[a_class] = math.exp(-factor * integral)
-
-        self.results.dry_depletions.insert(nuclide, values)
-
-    def __calculate_wet_depletions(self, nuclide: str) -> None:
-        """РБ-134-17, p. 28, (16)"""
-
-        windspeeds = self.input.extreme_windspeeds
-        dist = self.input.distance
-        detachment = self.results.sediment_detachments[nuclide]
-        values = dict()
-
-        for a_class in pasquill_gifford_classes:
-            values[a_class] = math.exp(
-                -detachment * dist / windspeeds[a_class]
-            )
-
-        self.results.wet_depletions.insert(nuclide, values)
-
-    def __calculate_full_depletions(self, nuclide: str) -> None:
-        """РБ-134-17, p. 29, (18)"""
-
-        rad = self.results.rad_depletions[nuclide]
-        dry = self.results.dry_depletions[nuclide]
-        wet = self.results.wet_depletions[nuclide]
-        values = dict()
-
-        for a_class in pasquill_gifford_classes:
-            values[a_class] = rad[a_class] * dry[a_class] * wet[a_class]
-
-        self.results.full_depletions.insert(nuclide, values)
